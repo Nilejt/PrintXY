7baae57f5eac0d6c6162d7f3799f8a61f500ff21
Nile Tate <Nilejt@gmail.com>
2015-07-06 14:18:49 -0400

    Whats this code do?
    I dont know what to do with my hands.

---
+++ PrintXY/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintXY
+{
+    public class Point
+    
+    {
+        public int X {get; set;}
+
+        public int Y {get; set;}
+
+        public double Distance
+        {
+            get { return Math.Sqrt(X * X + Y * Y); }
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("({0}, {1}) is {2} from the origin", X, Y, Distance);
+        }
+    }
+}
